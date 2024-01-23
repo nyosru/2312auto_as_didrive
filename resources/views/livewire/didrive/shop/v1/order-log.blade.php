995f716bb8806b95a5a417a61329ae93003ac318
Sergey <1@php-cat.com>
2024-01-23 10:16:41 +0500

    21 дата в логах up

--- resources/views/livewire/didrive/shop/v1/order-log.blade.php
+++ resources/views/livewire/didrive/shop/v1/order-log.blade.php
@@ -10,7 +10,11 @@
     @if($show)
         <div class="bg-gradient-to-br from-white to-orange-100 border-l-[10px] border-orange-300 pb-3 pl-2">
             @foreach( $data as $log )
-                <b>{{ $log->created_at->format('d.m H:i') }}</b>
+                {{-- если год разный то показываем, иначе только месяц и дата--}}
+                <abbr
+                    title="{{ $log->created_at->format('d.m.Y H:i:s') }}"><b>{{ $log->created_at->format('d.m') }}@if( date('Y') != $log->created_at->format('Y') )
+                            .{{ $log->created_at->format('Y') }}
+                        @endif {{ $log->created_at->format('H:i') }}</b></abbr>
                 {{--            {{ $log->model }} /--}}
                 {{--            {{ $log->type }} /--}}
                 {!! $log->msg !!}
